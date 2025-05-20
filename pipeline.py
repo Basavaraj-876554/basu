19e2910e5ca80f3e141218adeb4fcdde7190a0e2
Basavaraj-876554 <basavarajsultanpure84@gmail.com>
2025-05-20 16:11:13 +0530

    jenkins_pipeline

---
+++ pipeline.py
@@ -0,0 +1,13 @@
+pipeline {
+    agent any
+    stages {
+        stage('Checkout') {
+            steps {
+                sh {
+                    echo 'Checking out the code...'
+                
+                }
+            }
+        }
+    }
+}
